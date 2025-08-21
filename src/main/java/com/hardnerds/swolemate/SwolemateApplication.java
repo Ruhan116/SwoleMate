7c2a4d3ed362ece37e5073fd75683fba9b412415
Ruhan116 <138850716+Ruhan116@users.noreply.github.com>
2025-08-21 23:17:03 +0600

    Spring Boot Setup

---
+++ src/main/java/com/hardnerds/swolemate/SwolemateApplication.java
@@ -0,0 +1,13 @@
+package com.hardnerds.swolemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwolemateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwolemateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hardnerds/swolemate/SwolemateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hardnerds.swolemate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwolemateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
